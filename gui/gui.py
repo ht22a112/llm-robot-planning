795aabb01ba29ee63377d4fa926901f424f0ed37
Yamada Ryoga <ht22a112@oecu.jp>
2024-06-21 19:10:18 +0900

    左右２つのフィールドを作成し、２つのログを流せるように

--- gui/gui.py
+++ gui/gui.py
@@ -5,23 +5,30 @@ class LLMRobotPlannerInfoView(UserControl):
         super().__init__(*args, **kwargs)
         self.page = page
         self.expand = True
-        self.info_view = ListView(auto_scroll=True)
-
+        self.left_info_view = Container(content=ListView(auto_scroll=True), expand=True)
+        self.right_info_view = Container(content=ListView(auto_scroll=True), expand=True)
         # self.page.add(
         #     ElevatedButton("update", on_click=lambda e: self.update_gui())
         # )
         
     def build(self):
         return Container(
-            content=self.info_view
+            content=Row(
+                controls=[self.left_info_view, VerticalDivider(), self.right_info_view]
+            )
         )
 
-    def append(self, content: str):
-        self.info_view.controls.append(
+    def append_left(self, content: str):
+        self.left_info_view.content.controls.append(
             Text(value=content)
         )
-        self.info_view.update()
+        self.left_info_view.update()
 
+    def append_right(self, content: str):
+        self.right_info_view.content.controls.append(
+            Text(value=content, text_align="end")
+        )
+        self.right_info_view.update()
 
 
     

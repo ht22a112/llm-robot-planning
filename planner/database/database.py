45e29ed01bc2919d320b47c910a451ee1e8412c8
Yamada Ryoga <ht22a112@oecu.jp>
2024-10-24 18:27:30 +0900

    履歴データクラス（Task, Job）の削除、databaseの操作メソッドの追加

--- planner/database/database.py
+++ planner/database/database.py
@@ -1,54 +1,54 @@
-from typing import Optional, List, Literal
-
-class Task():
-    def __init__(
-        self, 
-        description: str,
-        detail: str,
-        required_info: list[str] = [],
-    ):
-        self.description: str = description
-        self.detail: str = detail
-        self.required_info = required_info
-        self.commands = {}
-        #self.done = False
-        
-    def __str__(self):
-        return f"[description:{self.description}, required_info:{self.required_info}]"
-    def __repr__(self):
-        return self.__str__()
-    
-    def to_dict(self):
-        return {
-            "description": self.description,
-            "detail": self.detail,
-            "required_info": self.required_info
-        }
-        
-class JobRecord():
-    def __init__(
-        self,
-        instruction: str,
-        tasks: list[Task]
-    ):
-        self.instruction: str = instruction
-        self.tasks: list[Task] = tasks
+from typing import Optional, List, Literal, Dict, Any
+from planner.database.data_type import Location, JobRecord, TaskRecord
 
 
-class Database():
+class MemoryDatabase():
     def __init__(self):
         self.records = {}
         self.current_job: Optional[JobRecord] = None
 
 from planner.database.sqlite import SQLiteInterface
+from planner.database.sqlite import LocationKnowledge, ObjectKnowledge, ActionHistory
 
 class DatabaseManager():
     def __init__(self):
         db_path = "planner/database/db/test.db"
-        self._memory_db = Database()
-        self._sql_db = SQLiteInterface(db_path)
+        self._memory_db = MemoryDatabase()
+        self._sqlite_interface = SQLiteInterface(db_path)
         
-    def start_new_job(self, instruction: str, tasks: list[Task]):
+        #
+        self._action_history = ActionHistory(self._sqlite_interface)
+        self._location_knowledge = LocationKnowledge(self._sqlite_interface)
+        #self.obj_db = ObjectKnowledge(self._sqlite_interface)
+        
+        #
+        self._init_helper()
+        
+    def _init_helper(self):
+        # TODO: あとで削除 テスト用
+        self.add_location_knowledge(
+            location_id="玄関_001",
+            location_name="玄関",
+            description="家の玄関",
+            x=1.0,
+            y=2.0,
+            z=0.0)
+        self.add_location_knowledge(
+            location_id="キッチン_001",
+            location_name="キッチン",
+            description="家のキッチン",
+            x=4.0,
+            y=5.0,
+            z=0.0)
+        self.add_location_knowledge(
+            location_id="トイレ_001",
+            location_name="トイレ",
+            description="家のトイレ",
+            x=7.0,
+            y=8.0,
+            z=0.0)
+        
+    def start_new_job(self, instruction: str, tasks: list[TaskRecord]):
         """
         Args:
             instruction: str ユーザーからの指示（最初にロボットに与える命令）
@@ -60,9 +60,9 @@ class DatabaseManager():
             tasks=tasks
         )
         
-    def get_current_job(self) -> JobRecord | None:
+    def get_current_job(self) -> Optional[JobRecord]:
         return self._memory_db.current_job
-
+    
     def log_robot_action(
         self,
         action: str,
@@ -73,7 +73,7 @@ class DatabaseManager():
         z: Optional[float],
         timestamp: Optional[float] = None
     ):
-        self._sql_db.log_robot_action(
+        self._action_history.log_robot_action(
             action=action,
             status=status,
             details=details,
@@ -82,6 +82,31 @@ class DatabaseManager():
             z=z,
             timestamp=timestamp
         )
+    
+    def add_location_knowledge(
+        self,
+        location_id: str,
+        location_name: str,
+        description: str,
+        x: float,
+        y: float,
+        z: float
+    ):
+        self._location_knowledge.add(
+            location_id=location_id,
+            location_name=location_name,
+            description=description,
+            x=x,
+            y=y,
+            z=z
+        )
         
-        
-        
+    def get_all_actions(self) -> List[Dict[str, Any]]:
+        return self._action_history.get_all()
+
+    def get_all_known_locations(self) -> List[Location]:
+        return self._location_knowledge.get_all()
+    
+    
+    
+    

d61eeccf41e2651772b1bbacc87ffae21b3374c2
Yamada Ryoga <ht22a112@oecu.jp>
2024-10-24 18:28:50 +0900

    locationをデータベースから取得するように変更

--- planner/task_service.py
+++ planner/task_service.py
@@ -1,20 +1,24 @@
 from typing import List, Union, Dict
 from planner.llm.gen_ai import UnifiedAIRequestHandler as LLM
+from planner.database.database import DatabaseManager
 from prompts.utils import get_prompt
 
-from planner.database.database import Task # TODO: 後に削除
+from planner.database.data_type import TaskRecord # TODO: 後に削除
 
 from logger.logger import LLMRobotPlannerLogSystem
 log = LLMRobotPlannerLogSystem()
 
 class TaskService():
-    def __init__(self, llm: LLM):
-        self.llm = llm
-    
+    def __init__(self, llm: LLM, db: DatabaseManager):
+        self._llm: LLM = llm
+        self._db = db
     # 後で削除
     def _get_all_knowledge_names(self) -> List[str]:
         return ["オブジェクト（物、物体）の位置", "場所の位置情報", "周囲に居る人の性別および名前に関する情報", "自身の過去の行動に関する情報"]
     
+    def _get_all_location_knowledge_names(self) -> List[str]:
+        return list(set([location.name for location in self._db.get_all_known_locations()]))
+        
     def split_task(self, task_description: str, task_detail: str, cmd_disc_list: List[str], action_history: str) -> dict:
         
         # コマンド一覧の生成
@@ -33,12 +37,13 @@ class TaskService():
                 "task_description": task_description,
                 "task_detail": task_detail,
                 "command_discription": command_discription,
-                "action_history": action_history
+                "action_history": action_history,
+                "location_info": str(self._get_all_location_knowledge_names())
             },
             symbol=("{{", "}}")
         )
         
-        response = self.llm.generate_content_v2(
+        response = self._llm.generate_content_v2(
             prompt=prompt, 
             response_type="json", 
             convert_type="dict",
@@ -46,15 +51,14 @@ class TaskService():
         )
         return response
         
-        
-        
-    def split_instruction(self, instruction: str) -> List[Task]:
+
+    def split_instruction(self, instruction: str) -> List[TaskRecord]:
         """
         Args:
             instruction: str ユーザーからの指示（最初にロボットに与える命令）
         
         Returns:
-            List[Task]: ロボットに与える命令のリスト
+            List[TaskRecord]: ロボットに与える命令のリスト
         """
         
         obtainable_information_list = "".join(f'・{name}\n' for name in self._get_all_knowledge_names())
@@ -67,7 +71,7 @@ class TaskService():
             symbol=("{{", "}}")
         )
         
-        response = self.llm.generate_content_v2(
+        response = self._llm.generate_content_v2(
             prompt=prompt,
             response_type="json",
             convert_type="dict",
@@ -75,7 +79,7 @@ class TaskService():
         )
             
         return [
-            Task(
+            TaskRecord(
                 description=task.get("description"), 
                 detail=task.get("detail"),
                 required_info=task.get("required information")
